75a1d73231d23de3fcc008f1fe1fe814eafbc2dc
Oriel Zambrano <oristides@gmail.com>
2016-11-22 13:01:08 -0200

    Upgrade for GetFeatures
    An little improvement  to support multitracks and playlist from getPlaylistSongs() function.

--- R/getFeatures.R
+++ R/getFeatures.R
@@ -1,33 +1,41 @@
-#'Get audio feature information for a single track identified by its unique Spotify ID.
+'Get audio feature information for a single (or multiple) tracks identified by its unique Spotify ID.
 #'
 #'
-#'function to get audio feature information for a single track identified by its unique Spotify ID.
+#'function to get audio feature information for a single or multiple tracks identified by its unique Spotify ID's but also from a list of Ids or even a playlist with GetPlaylist function
 #'@param spotify_ID The Spotify ID for the track.
 #'@param token An OAuth token created with \code{spotifyOAuth}.
 #'@export
 
-getFeatures<-function(spotify_ID,token){
-  req <- httr::GET(paste0("https://api.spotify.com/v1/audio-features/",spotify_ID), httr::config(token = token))
+GetFeatures <-function(songs, token) {
+  if(class(songs)=="data.frame") { songsnames <-songs$id } else { songsnames<-songs}
+  req <- httr::GET(paste0("https://api.spotify.com/v1/audio-features//?ids=",paste0(songsnames, collapse=",")), httr::config(token = keys))
   json1<-httr::content(req)
-  dados=data.frame(id=json1$id,
-                   danceability=json1$danceability,
-                   energy=json1$energy,
-                   key=json1$key,
-                   loudness=json1$loudness,
-                   mode=json1$mode,
-                   speechiness=json1$speechiness,
-                   acousticness=json1$acousticness,
-                   instrumentalness=json1$instrumentalness,
-                   liveness=json1$liveness,
-                   valence=json1$valence,
-                   tempo=json1$tempo,
-                   duration_ms=json1$duration_ms,
-                   time_signature=json1$time_signature,
-                   uri=json1$uri,
-                   analysis_url=json1$analysis_url,stringsAsFactors = F)
-  return(dados)
-}
-
+  json2<-jsonlite::fromJSON(jsonlite::toJSON(json1))
+  
+  #create columns
+  id<-unlist(json2$audio_features$id)
+  danceability <-unlist(json2$audio_features$danceability)
+  energy<-unlist(json2$audio_features$energy)
+  key<-unlist(json2$audio_features$key)
+  loudness<-unlist(json2$audio_features$loudness)
+  mode<-unlist(json2$audio_features$mode)
+  speechiness<-unlist(json2$audio_features$speechiness)
+  acousticness<-unlist(json2$audio_features$acousticness)
+  instrumentalness  <-unlist(json2$audio_features$instrumentalness)
+  liveness <-unlist(json2$audio_features$liveness)
+  valence<-unlist(json2$audio_features$valence)
+  tempo<-unlist(json2$audio_features$tempo)
+  duration_ms<-unlist(json2$audio_features$duration_ms)
+  time_signature<-unlist(json2$audio_features$time_signature)
+  uri<-unlist(json2$audio_features$uri)
+  analysis_url  <-unlist(json2$audio_features$analysis_url)
+  
+  #columns to data.frame
+  dados<-data.frame(id,danceability,energy,key,loudness, 
+                    mode, speechiness, acousticness, instrumentalness, liveness, valence,
+                    tempo, duration_ms,time_signature, uri, analysis_url, 
+                    stringsAsFactors = F) 
+  return(dados) }
 
 
 
